c5fcbe060a43fd473e6cd6d944f227b03933a3c9
Tegomlee <46428583+Tegomlee@users.noreply.github.com>
2024-07-31 14:40:10 -0400

    Added the main entry point and the git stuff

---
+++ main.py
@@ -0,0 +1,13 @@
+# Imports
+from src.engine import Engine
+
+
+# Main function
+def main():
+    engine = Engine()
+    engine.run()
+
+
+# Main entry point
+if __name__ == '__main__':
+    main()
---
+++ src/engine/__init__.py
@@ -0,0 +1,6 @@
+# Imports for the package
+from .engine import Engine
+
+
+# Acts as like an explicit includer
+__all__ = ['Engine']
